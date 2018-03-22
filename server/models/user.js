dd1bac795fe7ea01adc47fface19597a349435ac
Achowba <princeatauba@gmail.com>
2018-03-22 20:05:59 +0100

    added files

---
+++ server/models/user.js
@@ -0,0 +1,15 @@
+let mongoose = require('mongoose');
+
+// setup user model
+let User = mongoose.model('User', {
+    email: {
+        type: String,
+        required: true,
+        minLength: 5,
+        trim: true
+    }
+});
+
+module.exports = {
+    User
+};
---
+++ server/server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const bodyParser =require('body-parser');
+const { ObjectID } = require('mongodb');
+
+const { mongoose } = require('./db/mongoose-config');
+const { Todo } = require('./models/todo');
+const { User } = require('./models/user');
+
+const port = process.env.port || 2019;
+
+let app = express();
+
+app.use(bodyParser.json());
+
+// create route for posting data to the api
+app.post('/todos', (req, res) => {
+    console.log(req.body);
+
+    // create a todo from the data that comes from the post request
+    let todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+// get all todos
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos})
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+// get a single tod0 item
+app.get('/todos/:id', (req, res) => {
+    let todoId = req.params.id;
+    if (ObjectID.isValid(todoId) === false) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(todoId).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+
+        res.send({todo});
+    }).catch((e) => {
+        res.status(404).send(e);
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server started on port: ${port}`)
+});
+
+module.exports = {
+    app
+};
